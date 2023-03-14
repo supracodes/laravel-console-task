12bc1efb8de5a5d0b5ce741ac89c322a3c882dfe
Supra Codes <dev@supra.codes>
2023-03-14 15:34:24 +0700

    chore: psr namespace

--- src/LaravelConsoleTaskServiceProvider.php
+++ src/LaravelConsoleTaskServiceProvider.php
@@ -11,7 +11,7 @@ declare(strict_types=1);
  *  file that was distributed with this source code.
  */
 
-namespace NunoMaduro\LaravelConsoleTask;
+namespace SupraCodes\LaravelConsoleTask;
 
 use Illuminate\Console\Command;
 use Illuminate\Support\ServiceProvider;
